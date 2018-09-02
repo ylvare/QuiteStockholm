3d700af0f0c89ead9b293b2d941d0c784be93702
ylvare <ylva.rehnberg@gmail.com>
2018-09-02 16:45:43 +0200

    =now with nice menu

---
+++ quite_stockholm/src/components/Header/CategorySelect/CategorySelect.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import ScrollMenu from 'react-horizontal-scrolling-menu';
+import './CategorySelect.css';
+// list of items
+const list = [
+  { name: 'Fika' },
+  { name: 'Äta' },
+  { name: 'AW' },
+  { name: 'Läsa/Skriva' },
+  { name: 'Bada' }
+];
+
+// One item component
+// selected prop will be passed
+const MenuItem = ({ text, selected }) => {
+  return (
+    <div
+      className="menu-item"
+    >
+      {text}
+    </div>
+  );
+};
+
+// All items component
+// Important! add unique key
+export const Menu = (list) => list.map(el => {
+  const { name } = el;
+
+  return (
+    <MenuItem
+      text={name}
+      key={name}
+    />
+  );
+});
+
+
+const Arrow = ({ text, className }) => {
+  return (
+    <div
+      className={className}
+    >{text}</div>
+  );
+};
+
+
+const ArrowLeft = Arrow({ text: '<', className: 'arrow-prev' });
+const ArrowRight = Arrow({ text: '>', className: 'arrow-next' });
+
+class CategorySelect extends Component {
+  state = {
+    selected: 'Fika'
+  };
+
+  onSelect = key => {
+    this.setState({ selected: key });
+  }
+
+
+  render() {
+    const { selected } = this.state;
+    // Create menu from items
+    const menu = Menu(list, selected);
+
+    return (
+      <div className="CategorySelect">
+        <ScrollMenu
+          data={menu}
+          arrowLeft={ArrowLeft}
+          arrowRight={ArrowRight}
+          selected={selected}
+          onSelect={this.onSelect}
+          translate="0"
+          itemClassActive ="active"
+        />
+      </div>
+    );
+  }
+}
+
+export default CategorySelect
--- quite_stockholm/src/components/Header/Header.js
+++ quite_stockholm/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import '../../resources/CSS/GeneralStyles.css'
 import './Header.css'
 import litenLogga from "../../resources/images/litenlogga.png"
+import CategorySelect from './CategorySelect/CategorySelect'
 
 class Header extends Component {
 
@@ -10,9 +11,12 @@ class Header extends Component {
       <header>
         <div className="logo">
           <h1 className="desctop"> <span className="color1">Quite></span><span className="color2 desctop">Stockholm</span></h1>
-          <img src={litenLogga} className="mobile" alt="liten logga" id="logo"/>
+          <h1 className="mobile"> <span className="color1">Quite</span></h1>
         </div>
         <div className="navigation">
+          <div className="category-select">
+            <CategorySelect />
+          </div>
           <a href="index.html#about"><span className="desctop">Om appen</span><i id="questionMark"className="mobile fas fa-info"></i></a>
           <a href="index.html#tip"><span className="desctop">Tipsa</span><i className="mobile fas fa-volume-down"></i></a>
         </div>
